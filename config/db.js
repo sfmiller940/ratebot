36ef68a1020c141d591665884ea065bf2ec81d00
sfmiller940 <nineforty@gmail.com>
2017-05-22 17:45:50 -0700

    Updated mongo connection settings for openshift.

--- config/db.js
+++ config/db.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 // default to a 'localhost' configuration:
 var connection_string = '127.0.0.1:27017/ratebot';
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+if(process.env.OPENSHIFT_MONGODB_DB_HOST){
+  connection_string = 'mongodb://' +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+  process.env.OPENSHIFT_MONGODB_DB_PORT;
 }
 
 mongoose.connect(connection_string);
