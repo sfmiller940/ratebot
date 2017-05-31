d7c50e28479953cf81d5b382adba86eb7eb2e593
sfmiller940 <nineforty@gmail.com>
2017-05-31 14:16:13 -0700

    Frontend JS cleanup.

--- public/js/graph.js
+++ public/js/graph.js
@@ -16,96 +16,46 @@
       Plotly.Plots.resize(gd);
     };
 
-    $.get(
-      '/coins',
-      {},
-      function(coins){
-        coins.sort().forEach(function(coin){
-          $('#coinSelector select').append('<option value="'+coin+'">'+coin+'</option>');
-        });
-        updateGraph();
-      }
-    );
-    $('#coinSelector select').change(updateGraph);
-
-    var aveLengthInput = $('#aveLength');
-    var aveLengthWrap = $('#movingAve');
-    $('#addAveLength').click(function(){
-      traces.push( aveRate( aveLengthInput.val() , 'rgb(125,255,255)' ) );
-      var aveIndex = traces.length - 1;
-      Plotly.addTraces(gd, traces[ aveIndex ]);
-      aveLengthWrap.prepend('<label><input type="checkbox" name="ave'+aveIndex+'" id="ave'+aveIndex+'" value="'+ aveIndex +'" checked>'+aveLengthInput.val() +'</label>');
-      aveLengthInput.val('');
-
-      aveLengthWrap.find('#ave'+aveIndex).change(function(){
-        var removeInd = parseInt( $(this).val() );
-        Plotly.deleteTraces( gd, removeInd );
-        aveLengthWrap.find('input[type="checkbox"]').each(function(){
-          if( $(this).val() > removeInd ) $(this).val( $(this).val() - 1 );
-        });
-        traces.splice(removeInd,1);
-        $(this).parent().remove();
-      });
-    });
+    initGraph();
+    function initGraph(){
+      $.get(
+        '/coins',
+        {},
+        function(coins){
+          coins.sort().forEach(function(coin){
+            $('#coinSelector select').append('<option value="'+coin+'">'+coin+'</option>');
+          });
+          updateGraph();
+        }
+      );
+    }
 
     function updateGraph(){
+      var coin = $('#coinSelector select').val();
+
       $.get(
-        $('#coinSelector select').val(),
+        '/' + coin,
         {},
-        function(data) {
-
-          var dates = data.map(function(slice){ 
-            return $.format.date( new Date(slice.created_at), "yyyy-MM-dd HH:mm:ss"); 
-          });
-
-          var rates = data.map(function(slice){ return parseFloat(slice.rate); });  
+        function(rates) {
 
           traces[0]={
-            x: dates,
-            y: rates,
+            x: rates.map(function(slice){ return $.format.date( new Date(slice.created_at), "yyyy-MM-dd HH:mm:ss"); }),
+            y: rates.map(function(slice){ return parseFloat(slice.rate); }),
             type: 'scatter',
             marker: {
               color: 'rgba(125,125,255,1.0)'
             },
           };
 
-          var layout = {
-            title: $('#coinSelector select').val() + ' Prices and Rates',
-            paper_bgcolor: 'rgba(245,246,249,1)',
-            plot_bgcolor: 'rgba(245,246,249,1)',
-            showlegend: false,
-            annotations: [],
-            xaxis : { type:'date' },
-            yaxis1: {
-              tickfont: {color: 'rgb(125, 125, 255)'},
-              title: 'Rate',
-              titlefont: {
-                color: 'rgb(125,125,255)',
-              }
-            },
-            yaxis2: {
-              tickfont: {color: 'rgb(255, 125, 125)'},
-              overlaying: 'y',
-              side: 'right',
-              title: 'Price',
-              titlefont:{
-                color: 'rgb(255,125,125)',
-              },
-            }
-          };
-
-          Plotly.newPlot(gd, Array(traces[0]), layout);
-
-          var string='https://poloniex.com/public?command=returnChartData&currencyPair='
-              + ($('#coinSelector select').val() == 'BTC' ? 'USDT_BTC' : 'BTC_' + $('#coinSelector select').val()  )
+          $.get(
+            'https://poloniex.com/public?command=returnChartData'
+              + '&currencyPair=' + (coin == 'BTC' ? 'USDT_BTC' : 'BTC_' + coin )
               + '&start=' + parseInt( Date.parse( traces[0].x[0] ) / 1000 )
               + '&end='+ parseInt( Date.parse( traces[0].x[ traces[0].x.length -1 ] ) / 1000 )
-              + '&period=900';
-
-          $.get(
-            string,
+              + '&period=900',
             {},
             function(prices) {
+
               traces[1]={
                 x: prices.map(function(slice){ return $.format.date( new Date(slice.date * 1000), "yyyy-MM-dd HH:mm:ss"); }),
                 y: prices.map(function(slice){ return slice.weightedAverage }),
@@ -116,12 +66,37 @@
                   color: 'rgba(255, 125, 125, 1.0 )'
                 }
               };
-              Plotly.addTraces(gd,traces[1]);
+
+              Plotly.newPlot(gd,[traces[0],traces[1]], {
+                title: coin + ' Prices and Rates',
+                paper_bgcolor: 'rgba(245,246,249,1)',
+                plot_bgcolor: 'rgba(245,246,249,1)',
+                showlegend: false,
+                annotations: [],
+                xaxis : { type:'date' },
+                yaxis1: {
+                  tickfont: {color: 'rgb(125, 125, 255)'},
+                  title: 'Rate',
+                  titlefont: {
+                    color: 'rgb(125,125,255)',
+                  }
+                },
+                yaxis2: {
+                  tickfont: {color: 'rgb(255, 125, 125)'},
+                  overlaying: 'y',
+                  side: 'right',
+                  title: 'Price',
+                  titlefont:{
+                    color: 'rgb(255,125,125)',
+                  },
+                }
+              });
             }
           );
         }
       );
     }
+    $('#coinSelector select').change(updateGraph);
 
     function aveRate(aveLength, color){
       var dates = traces[0].x;
@@ -147,5 +122,25 @@
         },
       };
     }
+
+    var aveLengthInput = $('#aveLength');
+    var aveLengthWrap = $('#movingAve');
+    $('#addAveLength').click(function(){
+      traces.push( aveRate( aveLengthInput.val() , 'rgb(125,255,255)' ) );
+      var aveIndex = traces.length - 1;
+      Plotly.addTraces(gd, traces[ aveIndex ]);
+      aveLengthWrap.prepend('<label><input type="checkbox" name="ave'+aveIndex+'" id="ave'+aveIndex+'" value="'+ aveIndex +'" checked>'+aveLengthInput.val() +'</label>');
+      aveLengthInput.val('');
+
+      aveLengthWrap.find('#ave'+aveIndex).change(function(){
+        var removeInd = parseInt( $(this).val() );
+        Plotly.deleteTraces( gd, removeInd );
+        aveLengthWrap.find('input[type="checkbox"]').each(function(){
+          if( $(this).val() > removeInd ) $(this).val( $(this).val() - 1 );
+        });
+        traces.splice(removeInd,1);
+        $(this).parent().remove();
+      });
+    });
   });
 }(jQuery))
