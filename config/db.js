fb3724d13b7cb7479e53e02a71bbc22e6803c7ce
sfmiller940 <nineforty@gmail.com>
2018-03-08 21:06:31 -0800

    Fix DB connect string

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-var connection_string =  process.env.MONGODB_URI || 'localhost:27017/ratebot';
+var connection_string =  process.env.MONGODB_URI || 'mongodb://localhost:27017/ratebot';
 
 mongoose.connect(connection_string);
 
